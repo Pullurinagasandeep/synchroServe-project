c1bf2e197c5382148e140b39d94b182ef083c2bc
pullurinagasandeep <pullurinagasandeep@gmail.com>
2025-09-25 01:29:33 +0530

    added styling and ingredients list

--- script.js
+++ script.js
@@ -9,10 +9,11 @@ async function gettingCategoryList() {
   let content = ``;
   allCategoriesData.categories.map((category) => {
     content =
+      content +
       `<div class="category" id=${category.idCategory}>
-      <img class="category-img" src=${category.strCategoryThumb}>
-      <h3 class="category-heading">${category.strCategory}</h3>
-      </div>` + content;
+          <img class="category-img" src=${category.strCategoryThumb}>
+          <h3 class="category-heading">${category.strCategory}</h3>
+      </div>`;
   });
   root.innerHTML = content;
   allCategoriesData.categories.forEach((category) => {
@@ -40,14 +41,21 @@ async function gettingCategoryMeals(category) {
   singlemeals.forEach((singlemeal) => {
     categoryMeals.removeChild(singlemeal);
   });
+  const hero = document.querySelector(".hero");
+
+  const body = document.querySelector("main");
+  if (hero) {
+    body.removeChild(hero);
+  }
 
   let content = ``;
   singleCategoryData.meals.map((meal) => {
     content =
+      content +
       `<div class="single-meal" id=${meal.idMeal}>
-    <img class="meal-img" src="${meal.strMealThumb}">
-    <h3 class="meal-heading">${meal.strMeal}</h3>
-    </div>` + content;
+          <img class="meal-img" src="${meal.strMealThumb}">
+          <h3 class="meal-heading">${meal.strMeal}</h3>
+    </div>`;
   });
   categoryMeals.innerHTML = content;
 
@@ -71,20 +79,49 @@ async function gettingSingleMeal(id) {
     `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
   );
   const singleMealData = await singleMealResponse.json();
-  const body = document.querySelector("body");
-
+  const body = document.querySelector("main");
+  document.body.style.overflow = "hidden";
   // removing selected categories meals before showing new category meals
   const singlemeals = document.querySelectorAll(".single-meal-recipe");
   singlemeals.forEach((singlemeal) => {
     body.removeChild(singlemeal);
   });
+  // creating ingredients list
+  let ingredients =
+    "<h4 class='ingredients-heading'>Ingredients</h4><ul class='ingredients-list'>";
+  for (let i = 1; i <= 20; i++) {
+    const ingredient = singleMealData.meals[0][`strIngredient${i}`];
+    const measure = singleMealData.meals[0][`strMeasure${i}`];
+
+    if (ingredient) {
+      const imgsrcresponse = await fetch(
+        `https://www.themealdb.com/images/ingredients/${ingredient.replace(
+          " ",
+          "_"
+        )}.png`
+      );
+      const imgsrc = imgsrcresponse.url;
+      ingredients += `<li>
+      <span>${ingredient} - ${measure}</span>
+      <img src="${imgsrc}">
+      </li>`;
+    }
+  }
+  ingredients += "</ul>";
   // here if we work with inner html we are getting bug so it is prefered to this way
+
   singleMealData.meals.forEach((meal) => {
     const singleMeal = document.createElement("div");
     singleMeal.classList = "single-meal-recipe";
     singleMeal.setAttribute("id", meal.idMeal);
-    singleMeal.innerHTML = `<div style="display:flex;justify-content:space-between;align-items:center;"><img class="meal-image-recipe" src="${meal.strMealThumb}"><button style="padding:10px 20px; align-self:flex-start;position:fixed;top: 5%;right:10%;" onclick="hide()">X</button></div><h3 class="meal-heading-recipe">${meal.strMeal}</h3><p>${meal.strInstructions}</p>
-    <a href=${meal.strYoutube} target="_blank">link to youtube vedio</a>`;
+    singleMeal.innerHTML = `<div class="single-meal-wrapper" >
+    <img class="meal-image-recipe" src="${meal.strMealThumb}">
+     <h3 class="meal-heading-recipe">${meal.strMeal}</h3>
+<i class="fa-solid fa-xmark close-button" style="color: #000000;"  onclick="hide()"></i>
+    </div>
+    <p class="meal-instructions">${meal.strInstructions}</p>
+    <div>${ingredients}</div>
+    <a class="youtube-link" href=${meal.strYoutube} target="_blank">watch on Youtube</a>`;
 
     body.append(singleMeal);
   });
@@ -93,8 +130,9 @@ async function gettingSingleMeal(id) {
 // -------------------------------------------------------------------------------------------------------------------------------------------
 function hide() {
   const element = document.querySelector(".single-meal-recipe");
-  const body = document.querySelector("body");
+  const body = document.querySelector("main");
   body.removeChild(element);
+  document.body.style.overflow = "";
 }
 
 // -------------------------------------------------------------------------------------------------------------------------------------------
@@ -115,6 +153,12 @@ async function handleSearch(event) {
   singlemeals.forEach((singlemeal) => {
     categoryMeals.removeChild(singlemeal);
   });
+  const hero = document.querySelector(".hero");
+
+  const body = document.querySelector("main");
+  if (hero) {
+    body.removeChild(hero);
+  }
 
   let content = ``;
   mealData.meals.map((meal) => {
